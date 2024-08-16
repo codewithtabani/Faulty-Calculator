8c3f97681558cfb33b78087b42a3dd27cea42396
codewithtabani <mudassirtabani@gmail.com>
2024-08-16 16:04:49 +0500

    Faulty CAlculator

---
+++ index.js
@@ -0,0 +1,23 @@
+let number1 = prompt('Enter your first number')
+let operation = prompt('Enter operation')
+let number2 = prompt('Enter your second number')
+
+let random = Math.random()
+
+let obj = {
+    '+' : '-' ,
+    '*' : '+' ,
+    '-' : '/' ,
+    '/' : '**'
+}
+
+console.log(random);
+
+
+if (random > 0.1) {
+    alert(`The result is ${eval(`${number1} ${operation} ${number2}`)}`)
+}
+else{
+    operation = obj[operation]
+    alert(`The result is ${eval(`${number1} ${operation} ${number2}`)}`)
+}
